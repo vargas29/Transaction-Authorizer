3f0b3b7437858b7c9fb2039ddb010136fa20978e
Mafer <mavar09@gmail.com>
2021-12-19 23:21:32 -0500

    Add files via upload

---
+++ src/test/java/com/co/LuloBank/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.LuloBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
